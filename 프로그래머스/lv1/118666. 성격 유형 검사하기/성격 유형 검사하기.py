1821958b2755d57ae2952756ec08e0d988d592e9
최아정 <108112581+AJ15H@users.noreply.github.com>
2023-09-14 22:39:08 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.36 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,41 +1,41 @@
-def solution(survey, choices):
-    data = ["R", "T", "C", "F", "J", "M", "A", "N"]
-    score = [0 for str in data]
-    result = ''
+# def solution(survey, choices):
+#     data = ["R", "T", "C", "F", "J", "M", "A", "N"]
+#     score = [0 for str in data]
+#     result = ''
     
-    for i in range(len(survey)):
+#     for i in range(len(survey)):
         
-        if 4<choices[i]<=7:
-            score[data.index(survey[i][-1])] += choices[i]-4
-        if choices[i]==4:
-            pass
-        if 1<=choices[i]<4:
-            score[data.index(survey[i][0])] += 4-choices[i]
+#         if 4<choices[i]<=7:
+#             score[data.index(survey[i][-1])] += choices[i]-4
+#         if choices[i]==4:
+#             pass
+#         if 1<=choices[i]<4:
+#             score[data.index(survey[i][0])] += 4-choices[i]
             
     
-    if score[0]<score[1]:
-        result1=data[1]
-    else:
-        result1=data[0]
+#     if score[0]<score[1]:
+#         result1=data[1]
+#     else:
+#         result1=data[0]
         
-    if score[2]<score[3]:
-        result2=data[3]
-    else:
-        result2=data[2]
+#     if score[2]<score[3]:
+#         result2=data[3]
+#     else:
+#         result2=data[2]
         
-    if score[4]<score[5]:
-        result3=data[5]
-    else:
-        result3=data[4]
+#     if score[4]<score[5]:
+#         result3=data[5]
+#     else:
+#         result3=data[4]
     
-    if score[6]<score[7]:
-        result4=data[7]
-    else:
-        result4=data[6]
+#     if score[6]<score[7]:
+#         result4=data[7]
+#     else:
+#         result4=data[6]
         
-    result=result1+result2+result3+result4
+#     result=result1+result2+result3+result4
         
-    return result
+#     return result
 
 # def solution(survey, choices):
 #     data = ["R", "T", "C", "F", "J", "M", "A", "N"]
@@ -49,7 +49,7 @@ def solution(survey, choices):
 #         if choices[i]==4:
 #             pass
 #         if 1<=choices[i]<4:
-#             score[data.index(survey[i][0])] += choices[i]
+#             score[data.index(survey[i][0])] += 4-choices[i]
             
     
 #     if score[0]<score[1]:
@@ -75,24 +75,24 @@ def solution(survey, choices):
         
 #     return result
 
-# def solution(survey, choices):
-#     data = ["R", "T", "C", "F", "J", "M", "A", "N"]
-#     score = [0 for str in data]
-#     result = ''
+def solution(survey, choices):
+    data = ["R", "T", "C", "F", "J", "M", "A", "N"]
+    score = [0 for str in data]
+    result = ''
     
-#     for i in range(len(survey)):
+    for i in range(len(survey)):
         
-#         if 4<choices[i]<=7:
-#             score[data.index(survey[i][-1])] += choices[i]-4
-#         if choices[i]==4:
-#             pass
-#         if 1<=choices[i]<4:
-#             score[data.index(survey[i][0])] += choices[i]
+        if 4<choices[i]<=7:
+            score[data.index(survey[i][-1])] += choices[i]-4
+        if choices[i]==4:
+            pass
+        if 1<=choices[i]<4:
+            score[data.index(survey[i][0])] += 4-choices[i]
             
-#     for k in range(0,len(score),2) :
-#         if score[k]<score[k+1]:
-#             result+=data[k+1]
-#         else:
-#             result+=data[k]
+    for k in range(0,len(score),2) :
+        if score[k]<score[k+1]:
+            result+=data[k+1]
+        else:
+            result+=data[k]
             
-#     return result
+    return result
