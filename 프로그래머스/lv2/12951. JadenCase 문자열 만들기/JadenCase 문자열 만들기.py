5acc0fade941c5bde5c7fcca302457bd87e8ae14
최아정 <108112581+AJ15H@users.noreply.github.com>
2023-09-21 19:26:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    a=[]
+    s_li=list(s.split(' '))
+    
+    for i in s_li:
+        a.append(i.capitalize())
+    answer = ' '.join(a)
+        
+    
+    
+    return answer
